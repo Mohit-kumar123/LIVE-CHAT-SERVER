1b837b0909ac455c23a0436718680b3f8b6ed5fd
Mohit-kumar123 <mk0733920@gmail.com>
2025-03-19 15:34:58 +0530

    Updated backend code for Vercel deployment

--- Backend/index.js
+++ Backend/index.js
@@ -1,45 +1,38 @@
-let express=require('express');
-let app=express();
-const port=8000;
-var http=require("http").Server(app);
-var io=require("socket.io")(http);
+let express = require('express');
+let app = express();
+const path = require("path");
+const port = process.env.PORT || 8000;
 
-const path=require("path");
-const mainfile=path.join(__dirname, "../")
+var http = require("http").Server(app);
+var io = require("socket.io")(http, {
+    cors: {
+        origin: "https://mohit-kumar123.github.io", // Your frontend URL
+        methods: ["GET", "POST"]
+    }
+});
 
-app.use(express.static(mainfile));
+const activeusers = {};
 
-app.get("/",function(req,res){
-    res.sendFile(mainfile +"/index.html");
-}) 
+io.on("connection", (socket) => {
+    socket.on("new_user_joined", (username) => {
+        console.log("new user", username);
+        activeusers[socket.id] = username;
+        socket.broadcast.emit("user-joined", username);
 
-const activeusers={};
-io.on("connection",(socket)=>{
-    socket.on("new_user_joined",(username)=>{
-        console.log("new user",username);
-        activeusers[socket.id]=username;
-        socket.broadcast.emit("user-joined",username)
+        socket.on("disconnect", () => {
+            console.log("user-left", username);
+            socket.broadcast.emit("user-left", username);
+        });
+    });
 
+    socket.on("send", (message) => {
+        socket.broadcast.emit("recieve", {
+            message: message,
+            username: activeusers[socket.id]
+        });
+    });
+});
 
-        socket.on("disconnect",()=>{
-            console.log("user-left",username);
-            socket.broadcast.emit("user-left",username);
-            // delete activeusers[socket.id];
-        })
-
-
-    })
-
-    socket.on("send",(message)=>{
-        // console.log(message);
-
-        socket.broadcast.emit("recieve",{
-            message : message,
-            username : activeusers[socket.id]
-        })
-    })
-})
-
-http.listen(port,function(){
-    console.log(`server is listening at port ${port}`)
-})
+http.listen(port, function () {
+    console.log(`Server is listening at port ${port}`);
+});
